aa2fa084d7d875ef3b5a31a727f204fb6d05b44a
17sonu <97702823+17sonu@users.noreply.github.com>
2024-09-30 13:59:37 +0530

    Update index.js

--- node_frontend/index.js
+++ node_frontend/index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 // Route to call the Python API
 app.get('/start_capture', async (req, res) => {
     try {
-        const response = await axios.get('http://127.0.0.1:5000/start_capture');
+        const response = await axios.get('https://c476-2402-3a80-196c-6437-5b2-b041-33a0-fd73.ngrok-free.app/start_capture');
         res.send(response.data);
     } catch (error) {
         res.status(500).send("Error communicating with Python API");
